3a3160b574b813be33c3b27f4639b21d1aba4575
Dylan Almond <dylan@dylanalmond.net>
2024-02-25 03:23:50 +0000

    feat: added infinite scrolling

--- src/pages/WorkPage/WorkPage.tsx
+++ src/pages/WorkPage/WorkPage.tsx
@@ -3,10 +3,11 @@ import Card from '../../components/Card/Card'
 import { gql, useQuery } from '@apollo/client';
 import { Post } from '../../common/types';
 import Placeholder from '../../components/Placeholder/Placeholder';
+import { useRef, useEffect, useCallback } from 'react';
 
 const GET_ALL_POSTS = gql`
-  query getAllProjects {
-    projects(first: 9) {
+  query getAllProjects($first: Int = 12, $after: String = "") {
+    projects(first: $first, after: $after) {
       nodes {
         id
         title
@@ -20,22 +21,78 @@ const GET_ALL_POSTS = gql`
           }
         }
       }
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
     }
   }
 `;
 
 const WorkPage = () => {
-  const { loading, error, data } = useQuery(GET_ALL_POSTS);
+  const observerTarget = useRef(null);
+  const { loading, error, data, fetchMore } = useQuery(GET_ALL_POSTS, {
+    variables: {
+      first: 12,
+      after: ""
+    }
+  });
+
+  const fetchMoreData = useCallback(() => {
+    if (data && data.projects.pageInfo.hasNextPage && !loading) {      
+      fetchMore({
+        variables: {
+          first: 12,
+          after: data.projects.pageInfo.endCursor,
+        },
+        updateQuery: (prev, { fetchMoreResult }) => {
+          if (!fetchMoreResult) return prev;
+
+          return {
+            projects: {
+              __typename: 'ProjectConnection',
+              nodes: [...prev.projects.nodes, ...fetchMoreResult.projects.nodes],
+              pageInfo: fetchMoreResult.projects.pageInfo,
+            },
+          };
+        },
+      });
+    }
+  }, [data, fetchMore, loading]);
+
+  useEffect(() => {
+    let observerElem: HTMLElement;
+
+    const observer = new IntersectionObserver(
+      entries => {
+        if (entries[0].isIntersecting) {
+          fetchMoreData()
+        }
+      },
+      { threshold: 0 }
+    );
+  
+    if (observerTarget.current) {
+      observerElem = observerTarget.current;
+      observer.observe(observerElem);       
+    }
+
+    return () => {
+      if (observerElem) {
+        observer.unobserve(observerElem);
+      }
+    };
+  }, [fetchMoreData]);
 
   if (loading) {
-    // Return a loading state or skeleton screen
     return (
       <Wrapper>
         <CardGrid>
-          {[...Array(9)].map((_, i) =>
+          {[...Array(12)].map((_, i) =>
             <Placeholder key={i} />
           )}
         </CardGrid>
+        <div ref={observerTarget} />
       </Wrapper>
     );
   }
@@ -55,6 +112,7 @@ const WorkPage = () => {
           <Card key={post.id} post={post} />
         ))}
       </CardGrid>
+      <div ref={observerTarget} />
     </Wrapper>
   )
 }
